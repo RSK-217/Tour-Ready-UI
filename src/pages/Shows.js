f227909207ffc46ac1a50e24e98282255c0ed1e5
Rick Kersmarki <rkersmarki@gmail.com>
2022-12-17 10:58:32 -0600

    dates render correctly

--- src/pages/Shows.js
+++ src/pages/Shows.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
+import Moment from "moment";
 import { getAllShows } from "../api/UserData";
 import { getAllGroups } from "../api/UserData";
 import { ShowFilter } from "../Filters/ShowFilter";
@@ -10,22 +11,24 @@ export default function Shows({ currentUser }) {
     const [selctedGroup, setSelectedGroup] = useState([]);
     const [filter, setFilter] = useState('');
     const [dateFilter, setDateFilter] = useState('');
+
     
     const filterShows = () => {
-       if (filter > 0) {
-        return shows.filter(s => s.groupId === filter)
-       } else {
-        return shows
-       }
+        if (filter > 0) {
+            return shows.filter(s => s.groupId === filter)
+        } else {
+            return shows
+        }
     }
     console.log(filterShows())
-   
+    
     useEffect(() => {
         if(currentUser?.hasOwnProperty("id")) { 
-         getAllShows(currentUser).then((res) => {
-         setShows(res);
-        })};
-    }, [])
+            getAllShows(currentUser).then((res) => {
+                setShows(res);
+            })};
+        }, [])
+        
 
     useEffect(() => {
         if(currentUser?.hasOwnProperty("id")) { 
@@ -42,7 +45,7 @@ export default function Shows({ currentUser }) {
             <ShowFilter className="show-filter" groups={groups} value={filter} setFilter={setFilter} />
             {filterShows().map((show) => {
                 return (<li key={show.id} className='listed-show'>
-                        <Link className='show-link' to={`/show/${show.id}`}>{show.showDate} - {show.venue} - {show.cityName}, {show.state} {show.country}</Link>
+                        <Link className='show-link' to={`/show/${show.id}`}>{Moment(show.showDate).format('MM-DD-YYYY')} - {show.venue} - {show.cityName}, {show.state} {show.country}</Link>
                        </li>)
             })} 
 
